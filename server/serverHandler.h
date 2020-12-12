3b913731ae327333f3d698a66ebec52d57fd14d7
Serithipithy <58854430+Serithipithy@users.noreply.github.com>
2020-12-12 23:14:53 +0200

    Update serverHandler.h

--- server/serverHandler.h
+++ server/serverHandler.h
@@ -5,4 +5,7 @@
 #ifndef PROIECT_2_SERVERHANDLER_H
 #define PROIECT_2_SERVERHANDLER_H
 
+void parssingMsg ( char *message );
+int getCommand ( const char *message );
+void loginCommand ( const char message, char *messageToSend,int& userType );
 #endif //PROIECT_2_SERVERHANDLER_H
