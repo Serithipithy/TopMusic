01e354619ff23c077568d2f0dd1f5336313c95d4
Serithipithy <58854430+Serithipithy@users.noreply.github.com>
2020-12-27 18:19:37 +0200

    Update serverHandler.h

--- server/serverHandler.h
+++ server/serverHandler.h
@@ -4,15 +4,18 @@
 
 #ifndef SERVER_SERVERHANDLER_H
 #define SERVER_SERVERHANDLER_H
-
+#define MAX_CHR 2048
 #include <sqlite3.h>
 void parssingMsg (char *message );
 int getCommand ( const char *message );
 char* getUserName(const char *str);
 void loginCommand(sqlite3* db,char* sql, int &adminORuser,char* serverResponse,const char* clientMessage, int identificator);
 void registerCommand(sqlite3* db, int &adminORuser,char* serverResponse,const char* clientMessage);
+void addSongCommand(sqlite3* db,const char* clientMessage, char* serverResponse);
 // not in main
 static int callback(void *NowUsed, int argc, char **argv, char **azColName);
 static int callbackInsert(void *NotUsed, int argc, char **argv, char **azColName);
+
+int numberofappearances(char text[MAX_CHR], char character);
 
 #endif //SERVER_SERVERHANDLER_H
