861f869f17ae94f922cb1c3e9277a2e0d1908caa
Serithipithy <58854430+Serithipithy@users.noreply.github.com>
2020-12-27 16:37:21 +0200

    Update serverHandler.h

--- server/serverHandler.h
+++ server/serverHandler.h
@@ -6,10 +6,13 @@
 #define SERVER_SERVERHANDLER_H
 
 #include <sqlite3.h>
-
 void parssingMsg (char *message );
 int getCommand ( const char *message );
-
+char* getUserName(const char *str);
+void loginCommand(sqlite3* db,char* sql, int &adminORuser,char* serverResponse,const char* clientMessage, int identificator);
+void registerCommand(sqlite3* db, int &adminORuser,char* serverResponse,const char* clientMessage);
 // not in main
+static int callback(void *NowUsed, int argc, char **argv, char **azColName);
+static int callbackInsert(void *NotUsed, int argc, char **argv, char **azColName);
 
 #endif //SERVER_SERVERHANDLER_H
