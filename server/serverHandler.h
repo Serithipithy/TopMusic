48ef72f5ff9d3131b01f19e342f3ff94b971e05f
Serithipithy <58854430+Serithipithy@users.noreply.github.com>
2020-12-28 00:25:21 +0200

    Update serverHandler.h

--- server/serverHandler.h
+++ server/serverHandler.h
@@ -6,16 +6,22 @@
 #define SERVER_SERVERHANDLER_H
 #define MAX_CHR 2048
 #include <sqlite3.h>
-void parssingMsg (char *message );
+void parsingMsg (char *message );
 int getCommand ( const char *message );
-char* getUserName(const char *str);
-void loginCommand(sqlite3* db,char* sql, int &adminORuser,char* serverResponse,const char* clientMessage, int identificator);
+void getUserName(const char *str,char username[100]);
+void loginCommand(sqlite3* db,char* sql, int &adminORuser,char* serverResponse,const char* clientMessage, int identificator, char* username);
 void registerCommand(sqlite3* db, int &adminORuser,char* serverResponse,const char* clientMessage);
 void addSongCommand(sqlite3* db,const char* clientMessage, char* serverResponse);
+void restrictVoteCommand(sqlite3* db,char* userName,char right[4],char* serverResponse);
+void deleteSongCommand(sqlite3* db,char* clientMessage,char* serverResponse);
+void voteSongCommand(sqlite3* db,char* clientMessage,char* serverResponse);
+int canvote(sqlite3* db,char* user);
 // not in main
-static int callback(void *NowUsed, int argc, char **argv, char **azColName);
-static int callbackInsert(void *NotUsed, int argc, char **argv, char **azColName);
+static int callback(void *NowUsed, int argc, char **argv, char **azColName); // cu returnare rezultat
+static int callbackInsert(void *NotUsed, int argc, char **argv, char **azColName); // fara returnare rezultat
+static int callbackID(void *NowUsed, int argc, char **argv, char **azColName); // pentru a luat ID-ul melodiei
 
 int numberofappearances(char text[MAX_CHR], char character);
+void addInGenre(sqlite3* db,char ID[MAX_CHR],char genuri[200],char* serverResponse);
 
 #endif //SERVER_SERVERHANDLER_H
