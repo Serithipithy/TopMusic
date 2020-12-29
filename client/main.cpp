18b061ded469afc9e9690c47e4624d04adcd4d5d
Serithipithy <58854430+Serithipithy@users.noreply.github.com>
2020-12-29 17:11:59 +0200

    Add files via upload

--- client/main.cpp
+++ client/main.cpp
@@ -9,6 +9,7 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <fstream>
+#include <string>
 
 /* codul de eroare returnat de anumite apeluri */
 extern int errno;
@@ -16,25 +17,24 @@ extern int errno;
 /* portul de conectare la server*/
 int port;
 
-int main (int argc, char *argv[])
-{
-    int sd;			// descriptorul de socket
-    struct sockaddr_in server;	// structura folosita pentru conectare
-    char msgToSend[400];		// mesajul trimis
-    char msgRead[400];          // mesajul citit
+int main(int argc, char *argv[]) {
+    int sd;            // descriptorul de socket
+    struct sockaddr_in server;    // structura folosita pentru conectare
+    char msgToSend[2048];        // mesajul trimis
+    char msgRead[2048];          // mesajul citit
 
     /* exista toate argumentele in linia de comanda? */
-    if (argc != 3){
-        printf ("Sintaxa: %s <adresa_server> <port>\n", argv[0]);
+    if (argc != 3) {
+        printf("Sintaxa: %s <adresa_server> <port>\n", argv[0]);
         return -1;
     }
 
     /* stabilim portul */
-    port = atoi (argv[2]);
+    port = atoi(argv[2]);
 
     /* cream socketul */
-    if ((sd = socket (AF_INET, SOCK_STREAM, 0)) == -1){
-        perror ("Eroare la socket().\n");
+    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Eroare la socket().\n");
         return errno;
     }
 
@@ -44,41 +44,46 @@ int main (int argc, char *argv[])
     /* adresa IP a serverului */
     server.sin_addr.s_addr = inet_addr(argv[1]);
     /* portul de conectare */
-    server.sin_port = htons (port);
+    server.sin_port = htons(port);
 
     /* ne conectam la server */
-    if (connect (sd, (struct sockaddr *) &server,sizeof (struct sockaddr)) == -1){
+    if (connect(sd, (struct sockaddr *) &server, sizeof(struct sockaddr)) == -1) {
         perror("[client]Eroare la connect().\n");
         return errno;
     }
-    // TODO :mesaj de intampinare
-    while(1) {
+    // mesaj de intampinare
+    std::string header( 40, '=' );
+    char welcome[]="\n\n\n\t\t\t\t  Welcome to TopMusic!";
+    char somethingcute[]="\t\t\tWhere you find your favorite application \n\t\t\t\t\t   and \n\t\t\t  your favorite songs in the same place ";
+    printf ("%s\n%s\n\n\n\n",welcome,somethingcute);
+    while (1) {
         // citire comanda client
-        bzero (msgToSend, 400);
-        printf ("::: ");
-        fflush (stdout);
-        read (0, msgToSend, 400);
-        msgToSend[strlen(msgToSend)-1]='\0';
+        bzero(msgToSend, 2048);
+        printf(":: ");
+        fflush(stdout);
+        read(0, msgToSend, 2048);
+        msgToSend[strlen(msgToSend) - 1] = '\0';
 
         // trimitere mesaj catre server
-        if (write (sd, msgToSend, 400) <= 0){
-            perror ("[client]Eroare la write() spre server.\n");
+        if (write(sd, msgToSend, 2048) <= 0) {
+            perror("[client]Eroare la write() spre server.\n");
             return errno;
         }
 
         // asteapta pana cand primeste raspuns/mesaj de la server
-        bzero(msgRead,400);
-        if (read (sd, msgRead, 400) < 0){
-            perror ("[client]Eroare la read() de la server.\n");
+        bzero(msgRead, 2048);
+        if (read(sd, msgRead, 2048) < 0) {
+            perror("[client]Eroare la read() de la server.\n");
             return errno;
         }
 
         // afisare mesaj trimis de catre server
-        printf (">> %s\n", msgRead);
-        if (strcmp(msgRead,"Quit!\n") == 0) {
+        printf(">> %s\n", msgRead);
+        fflush(stdout);
+        if (strcmp(msgRead, "Quit!\n") == 0) {
             break;
         }
     }
 
-    close (sd);
+    close(sd);
 }
