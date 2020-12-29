c9449cd53f44b2b15a3d2b9aa86b8f9480da0f53
Serithipithy <58854430+Serithipithy@users.noreply.github.com>
2020-12-29 03:02:13 +0200

    Update serverHandler.h

--- server/serverHandler.h
+++ server/serverHandler.h
@@ -9,13 +9,19 @@
 void parsingMsg (char *message );
 int getCommand ( const char *message );
 void getUserName(const char *str,char username[100]);
-void loginCommand(sqlite3* db,char* sql, int &adminORuser,char* serverResponse,const char* clientMessage, int identificator, char* username);
+void loginCommand(sqlite3* db,char* sql, int &adminORuser,char* serverResponse,const char* clientMessage, int identificator);
 void registerCommand(sqlite3* db, int &adminORuser,char* serverResponse,const char* clientMessage);
 void addSongCommand(sqlite3* db,const char* clientMessage, char* serverResponse);
 void restrictVoteCommand(sqlite3* db,char* userName,char right[4],char* serverResponse);
 void deleteSongCommand(sqlite3* db,char* clientMessage,char* serverResponse);
 void voteSongCommand(sqlite3* db,char* clientMessage,char* serverResponse);
-int canvote(sqlite3* db,char* user);
+void addCommentCommand(sqlite3* db,char* clientMessage,char* serverResponse,char loggedIn[100]);
+void see_top_general(sqlite3* db, char* serverResponse);
+void see_users_command(sqlite3* db,char* clientMessage);
+void see_top_genre(sqlite3* db,char* clientMessage,char* serverResponse);
+void see_genres_command(sqlite3* db,char* serverResponse);
+int verify_vote_right(sqlite3* db,char user[100]);
+void erase_empty_tables(sqlite3* db);
 // not in main
 static int callback(void *NowUsed, int argc, char **argv, char **azColName); // cu returnare rezultat
 static int callbackInsert(void *NotUsed, int argc, char **argv, char **azColName); // fara returnare rezultat
